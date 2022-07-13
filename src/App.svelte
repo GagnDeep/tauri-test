d93efad164378dea52e8f3f020ebc4c0a40f3a5c
GagnDeep <dp.gagn@gmail.com>
2022-07-13 06:52:02 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import {os, fs} from '@tauri-apps/api'
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		console.log({
+			name: await os.arch()
+		})
+
+		const files = fs.BaseDirectory
+
+
+	})
+
+	export let name: string;
+</script>
+
+
+hellow worlddd!!!!
